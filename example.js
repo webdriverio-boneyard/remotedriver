f081fbf07947fc1de4a129436a3976e9056e4f1d
christian-bromann <contact@christian-bromann.com>
2016-09-15 19:14:28 +0200

    add demo script

---
+++ example.js
@@ -0,0 +1,55 @@
+var Chrome = require('./build/browser/chrome')
+var browser = new Chrome('localhost', 9223)
+
+var client = require('webdriverio').remote({
+    desiredCapabilities: {
+        browserName: 'chrome',
+        chromeOptions: {
+            debuggerAddress: 'localhost:9223'
+        }
+    }
+})
+
+client
+    /**
+     * initialise browser, starts Chrome browser, proxy and remote debugger instance
+     */
+    .call(browser.start.bind(browser))
+    /**
+     * attach Selenium session to Chrome browser
+     */
+    .init()
+    /**
+     * start network profiling
+     */
+    .call(browser.recordNetworkLog.bind(browser))
+    /**
+     * open google.com
+     */
+    .url('http://google.com')
+    /**
+     * stop network profiling, store HAR file on fs
+     * start cpu profiling
+     */
+    .call(function () {
+        browser.stopRecordNetworkLog('profile.har')
+        browser.profileCPU()
+    })
+    /**
+     * interact with the page
+     */
+    .setValue('#lst-ib', 'WebdriverIO')
+    .pause(1000) // let the page render
+    /**
+     * stop profiling and save .cpuprofile file
+     */
+    .call(function () {
+        return browser.stopCPUProfiling('profile.cpuprofile')
+    })
+    /**
+     * stop selenium session and kill Chrome
+     * because we attached the session to the browser it can't kill Chrome by itself
+     */
+    .end().then(function () {
+        browser.shutdown()
+    })
